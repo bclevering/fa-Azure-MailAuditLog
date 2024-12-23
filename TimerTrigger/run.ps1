676bd8fd1fe5cba4b7e579ec8c4fe0045e48f050
Clevering, L.E. (Bert) <b.clevering@sharevalue.nl>
2024-12-23 09:43:27 +0100

    mail audit app initial deployment

---
+++ TimerTrigger/run.ps1
@@ -0,0 +1,31 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Get the current universal time in the default string format
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+
+# The tenant name (orgname.onmicrosoft.com) set in the Function App configuration
+$tenant = $env:Tenant 
+
+try {
+    Connect-ExchangeOnline -ManagedIdentity -Organization $tenant
+    # Execute the EXO commands you want here
+}
+catch {    
+    # Implement error handling here    
+    throw $_
+}
+finally {
+    Disconnect-ExchangeOnline -Confirm:$false
+    Get-PSSession | Remove-PSSession
+}
+
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
---
+++ requirements.psd1
@@ -0,0 +1,13 @@
+# This file enables modules to be automatically managed by the Functions service.
+# See https://aka.ms/functionsmanageddependency for additional information.
+#
+@{
+    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'. 
+    # To use the Az module in your function app, please uncomment the line below.
+    #'Az' = '12.*'
+    'Az.Accounts' = '2.*'
+    'Az.ManagedServiceIdentity' = '1.*'
+    'ExchangeOnlineManagement' = '3.*'
+    'PowerShellGet' = '2.*'
+    'PackageManagement' = '1.*'
+}
